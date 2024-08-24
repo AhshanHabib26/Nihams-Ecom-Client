0ba8ac9e3418505ccfb111780345c34c487d3a59
AhshanHabib26 <habib.net27@gmail.com>
2024-08-24 10:23:54 +0600

    Banner Card Updated

--- src/components/client/BannerCard.tsx
+++ src/components/client/BannerCard.tsx
@@ -8,24 +8,28 @@ const BannerCard = () => {
   const banner = getCommonData();
 
   return (
-    <Container>
-      <div>
-        {banner.slice(7, 10).map((item) => (
-          <div key={item.id}>
-            <div>
-              <Image
-                src={item.image}
-                width={100}
-                height={100}
-                alt={item.title}
-              />
-              <h1>{item.title}</h1>
-              <Link href="/shop">Shop Now</Link>
+    <div>
+      <Container>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5  max-w-5xl mx-auto my-5">
+          {banner.slice(7, 10).map((item) => (
+            <div key={item.id}>
+              <div className="flex items-center bg-gray-100 p-4 gap-6 rounded shadow-lg">
+                <Image
+                  src={item.image}
+                  width={120}
+                  height={120}
+                  alt={item.title}
+                />
+                <div>
+                  <h1 className="text-lg font-semibold">{item.title}</h1>
+                  <Link className="text-orange-600" href="/shop">Shop Now</Link>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </Container>
+          ))}
+        </div>
+      </Container>
+    </div>
   );
 };
 
